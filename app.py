043cab201dac28a289b97cbb81e042677d8b77bd
shaybenjamin <jshayben@gmail.com>
2022-07-18 03:01:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ def index():
     global counter
     if request.method == "POST":
         counter+=1
-        return "Hmm, Plus 1 please "
+        return "DEPLOYED | Hmm, Plus 1 please "
     else:
-        return str(f"Our counter is: {counter} ")
+        return str(f"DEPLOYED | Our counter is: {counter} ")
 if __name__ == '__main__':
     app.run(debug=True,port=5000,host='0.0.0.0')
